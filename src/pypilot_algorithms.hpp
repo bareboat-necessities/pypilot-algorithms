a80f486f7d62c4934f60df6f9ff2eae2d6074b1a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 22:46:41 -0400

    Add public algorithms include

---
+++ src/pypilot_algorithms.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "pypilot_algorithms/angles.hpp"
+#include "pypilot_algorithms/filters.hpp"
+#include "pypilot_algorithms/control.hpp"
+#include "pypilot_algorithms/basic_pilot.hpp"
+#include "pypilot_algorithms/wind_pilot.hpp"
