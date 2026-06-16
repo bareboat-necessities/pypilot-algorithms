0a3fa174699c31fe8bc65c2749097a4669bc547a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 22:47:22 -0400

    Add basic pilot algorithm

---
+++ src/pypilot_algorithms/basic_pilot.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "control.hpp"
+
+namespace pypilot_algorithms {
+
+template<typename Real = float>
+struct BasicPilotGains {
+    Real P = Real(0);
+    Real D = Real(0);
+    Real DD = Real(0);
+    Real PR = Real(0);
+    Real FF = Real(0);
+};
+
+template<typename Real = float>
+struct BasicPilotInput {
+    Real heading_error_deg = Real(0);
+    Real headingrate_deg_s = Real(0);
+    Real headingraterate_deg_s2 = Real(0);
+    Real heading_command_rate_deg_s = Real(0);
+};
+
+template<typename Real = float>
+struct BasicPilotOutput {
+    Real command_norm = Real(0);
+    Real Pgain = Real(0);
+    Real Dgain = Real(0);
+    Real DDgain = Real(0);
+    Real PRgain = Real(0);
+    Real FFgain = Real(0);
+};
+
+template<typename Real = float>
+inline BasicPilotOutput<Real> compute_basic_pilot(const BasicPilotInput<Real>& in,
+                                                   const BasicPilotGains<Real>& gains,
+                                                   Real command_limit = Real(1)) {
+    BasicPilotOutput<Real> out;
+    out.Pgain = gains.P * in.heading_error_deg;
+    out.Dgain = gains.D * in.headingrate_deg_s;
+    out.DDgain = gains.DD * in.headingraterate_deg_s2;
+    out.PRgain = gains.PR * signed_sqrt_abs(in.heading_error_deg);
+    out.FFgain = gains.FF * in.heading_command_rate_deg_s;
+    out.command_norm = clamp_command(out.Pgain + out.Dgain + out.DDgain + out.PRgain + out.FFgain, command_limit);
+    return out;
+}
+
+} // namespace pypilot_algorithms
