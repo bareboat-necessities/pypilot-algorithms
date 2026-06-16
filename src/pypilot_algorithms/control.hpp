ec47d2d19d622911a06d66d923f2ae6cd46afdce
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 22:47:13 -0400

    Add control helpers

---
+++ src/pypilot_algorithms/control.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <math.h>
+#include "angles.hpp"
+
+namespace pypilot_algorithms {
+
+template<typename Real = float>
+struct GainContribution {
+    Real output = Real(0);
+    Real contribution = Real(0);
+};
+
+template<typename Real>
+inline Real sign(Real value) {
+    return value > Real(0) ? Real(1) : (value < Real(0) ? Real(-1) : Real(0));
+}
+
+template<typename Real>
+inline Real signed_sqrt_abs(Real value) {
+    return sign(value) * static_cast<Real>(sqrt(abs_real(value)));
+}
+
+template<typename Real>
+inline GainContribution<Real> apply_gain(Real gain, Real input) {
+    GainContribution<Real> out;
+    out.contribution = gain * input;
+    out.output = out.contribution;
+    return out;
+}
+
+template<typename Real>
+inline Real clamp_command(Real command, Real limit = Real(1)) {
+    return minmax(command, limit);
+}
+
+} // namespace pypilot_algorithms
