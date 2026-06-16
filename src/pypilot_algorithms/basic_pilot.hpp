c0c92ae60093b09bb8567b15b3d5be8c40d88706
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 10:00:23 -0400

    Add raw basic pilot compute

--- src/pypilot_algorithms/basic_pilot.hpp
+++ src/pypilot_algorithms/basic_pilot.hpp
@@ -33,16 +33,31 @@ struct BasicPilotOutput {
 
 template<typename Real = float>
 inline BasicPilotOutput<Real> compute_basic_pilot(const BasicPilotInput<Real>& in,
-                                                   const BasicPilotGains<Real>& gains,
-                                                   Real command_limit = Real(1)) {
+                                                  const BasicPilotGains<Real>& gains,
+                                                  CommandClamp clamp_mode,
+                                                  Real command_limit = Real(1)) {
     BasicPilotOutput<Real> out;
     out.Pgain = gains.P * in.heading_error_deg;
     out.Dgain = gains.D * in.headingrate_deg_s;
     out.DDgain = gains.DD * in.headingraterate_deg_s2;
     out.PRgain = gains.PR * signed_sqrt_abs(in.heading_error_deg);
     out.FFgain = gains.FF * in.heading_command_rate_deg_s;
-    out.command_norm = clamp_command(out.Pgain + out.Dgain + out.DDgain + out.PRgain + out.FFgain, command_limit);
+    Real command = out.Pgain + out.Dgain + out.DDgain + out.PRgain + out.FFgain;
+    out.command_norm = apply_command_clamp(command, clamp_mode, command_limit);
     return out;
+}
+
+template<typename Real = float>
+inline BasicPilotOutput<Real> compute_basic_pilot(const BasicPilotInput<Real>& in,
+                                                  const BasicPilotGains<Real>& gains,
+                                                  Real command_limit = Real(1)) {
+    return compute_basic_pilot(in, gains, CommandClamp::clamped, command_limit);
+}
+
+template<typename Real = float>
+inline BasicPilotOutput<Real> compute_basic_pilot_raw(const BasicPilotInput<Real>& in,
+                                                      const BasicPilotGains<Real>& gains) {
+    return compute_basic_pilot(in, gains, CommandClamp::raw);
 }
 
 } // namespace pypilot_algorithms
